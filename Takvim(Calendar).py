15a626d0f3f79c800468d155739ab7d500bf9e95
Mehmetali <118634815+mehm3tali@users.noreply.github.com>
2023-02-02 16:07:24 +0300

    Update Takvim(Calendar).py

--- Takvim(Calendar).py
+++ Takvim(Calendar).py
@@ -21,13 +21,3 @@ time.sleep(60)
 
 
 
-# import  # using module keyboard
-# while True:  # making a loop
-#     try:  # used try so that if user pressed other than the given key error will not be shown
-#         if keyboard.is_pressed('q'):  # if key 'q' is pressed 
-#             print('You Pressed A Key!')
-#             break  # finishing the loop
-#         else:
-#             pass
-#     except:
-#         break  # if user pressed a key other than the given key the loop will break
