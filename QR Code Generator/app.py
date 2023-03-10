e5cbf651ba996f64317a3e5646253547f8c8b230
Mehmetali <118634815+mehm3tali@users.noreply.github.com>
2023-03-10 15:36:51 +0300

    Add files via upload

---
+++ QR Code Generator/app.py
@@ -0,0 +1,8 @@
+import qrcode
+
+url = input("Enter URL: ")
+qr = qrcode.QRCode(version=1, box_size=10, border=5)
+qr.add_data(url)
+qr.make(fit=True)
+img = qr.make_image(fill_color='black', back_color='white')
+img.save('qrcode.png')
